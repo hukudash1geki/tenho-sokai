c951750ba2c2ffb2b38b1e2d94f40bcc70b480ed
hukudash1geki <fukudashgek81@gmail.com>
2023-07-30 05:49:54 +0900

    ダウンロードと展開のプログラム記入

--- app/controllers/tenho_sokais_controller.rb
+++ app/controllers/tenho_sokais_controller.rb
@@ -1,7 +1,6 @@
 class TenhoSokaisController < ApplicationController
   def index
-    url = 'https://tenhou.net/sc/raw/'
-    @log_urls = TenhouScraper.get_links_with_dat_sca(url)
+    log_urls = TenhouScraper.get_links_with_dat_sca
   end
 
   def create
--- app/lib/tenhou_scraper.rb
+++ app/lib/tenhou_scraper.rb
@@ -1,37 +1,33 @@
-require 'selenium-webdriver'
+require 'uri'
+require 'open-uri'
+require 'zlib'
 
 class TenhouScraper
-  def self.get_links_with_dat_sca(url)
-    log_urls = []
-
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--headless')
-    driver = Selenium::WebDriver.for :chrome, options: options
+  def self.get_links_with_dat_sca
+    url = URI.parse("https://tenhou.net/sc/raw/dat/scb2023072100.log.gz")
+    local_filename = File.basename(url.path)
+    current_directory = File.dirname(__FILE__)
+    save_path = File.join(current_directory, local_filename)
+    extracted_filename = local_filename.gsub('.gz', '')
 
     begin
-      driver.get(url)
-      wait = Selenium::WebDriver::Wait.new(timeout: 30)
-      wait.until { driver.execute_script('return document.readyState') == 'complete' }
-
-      page_source = driver.page_source
-      doc = Nokogiri::HTML(page_source)
-
-      doc.css("a").each do |link|
-        href = link['href']
-        if href && href.start_with?("dat/")
-          absolute_url = URI.join(url, href)
-          log_urls << absolute_url.to_s
-          sleep 1
+      URI.open(url) do |remote_file|
+        File.open(save_path, 'wb') do |local_file|
+          local_file.write(remote_file.read)
         end
       end
-    rescue StandardError => e
-      puts "Error occurred while scraping: #{e.message}"
-    ensure
-      driver.quit
-    end
 
-    puts "log_urls: #{log_urls.inspect}"
-    log_urls
+      Zlib::GzipReader.open(save_path) do |gz|
+        File.open(File.join(current_directory, extracted_filename), 'wb') do |unzipped_file|
+          unzipped_file.write(gz.read)
+        end
+      end
+
+      puts "ファイルをダウンロードしました: #{local_filename}"
+      puts "ファイルを展開しました: #{extracted_filename}"
+    rescue => e
+      puts "ダウンロード中にエラーが発生しました: #{e.message}"
+    end
   end
 end
 
