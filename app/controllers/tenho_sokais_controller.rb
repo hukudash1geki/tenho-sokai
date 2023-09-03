471c59d1d08e6ed21c97536dd5d7b61163f49835
hukudash1geki <fukudashgek81@gmail.com>
2023-09-03 23:51:27 +0900

    名前検索確認

--- app/controllers/tenho_sokais_controller.rb
+++ app/controllers/tenho_sokais_controller.rb
@@ -4,13 +4,7 @@ class TenhoSokaisController < ApplicationController
   end
 
   def show
-    username = params[:username]
-    @scblog = name_params
-
-    if @scblog.nil?
-      flash[:error] = "該当するユーザーが見つかりませんでした。"
-      redirect_to root_path # または適切なリダイレクト先に変更
-    end
+    @scblog = ScbLog.find(params[:id])
   end
 
   def search
